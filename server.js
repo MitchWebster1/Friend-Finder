a189df23ea7e9fbffe6c0f3376c9a18a71d034ab
Mitch Webster <mitch83083@gmail.com>
2019-07-31 19:01:21 -0400

    changed port to work with heroku

--- server.js
+++ server.js
@@ -2,31 +2,7 @@ const path = require('path')
 const fs = require('fs-extra')
 const express = require('express')
 const app = express()
-const port = 3000
-
-function displayHtml(res) {
-  fs.readFile(path.join(__dirname, 'home'), function(err, data) {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/html' })
-      return res.end('It borked Jim!')
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    return res.end(data)
-  })
-}
-
-const handleRequest = (req, res) => {
-  const { url } = req
-  console.log(url)
-  switch (url) {
-    case '/':
-      return displayHtml('home', res)
-    case '/survey':
-      return displayHtml('survey', res)
-    default:
-      displayHtml('home', res)
-  }
-}
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '/app/public')))
 
